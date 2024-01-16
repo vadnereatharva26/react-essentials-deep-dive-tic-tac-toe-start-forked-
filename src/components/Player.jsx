870cb76bf4118a7d859bf8400b7e21e4f6433cc9
CodeSandbox <support@codesandbox.io>
2024-01-16 10:12:39 +0000

    Sandbox snapshot commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // giving function as a parameter for state that depends on its previous state
+  }
+
+  function handleChange(event) {
+    //console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //let buttonCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    //buttonCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player-info">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
